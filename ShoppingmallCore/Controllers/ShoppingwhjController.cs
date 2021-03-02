61d6a901d0012cd831afd872867e985d1566b7ee
小杰 <@LAPTOP-SVTJLGVG>
2021-03-02 18:58:00 +0800

    王豪杰

---
+++ ShoppingmallCore/Controllers/ShoppingwhjController.cs
@@ -0,0 +1,54 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ShoppingmallCore.Controllers
+{
+    public class ShoppingwhjController : Controller
+    {
+        //预报
+        public IActionResult Yubao()
+        {
+            return View();
+        }
+        //钱包
+        public IActionResult Qianbao()
+        {
+            return View();
+        }
+        //收藏
+        public IActionResult Shouchang()
+        {
+            return View();
+        }
+
+        //我的地址
+        public IActionResult Wodedizhi()
+        {
+            return View();
+        }
+
+        //我的地址 新增
+        public IActionResult WodedizhiAdd()
+        {
+            return View();
+        }
+
+        //我的地址 修改
+        public IActionResult WodedizhiUpdate(int id)
+        {
+            ViewBag.id = id;
+            return View();
+        }
+
+
+        //仓库地址
+        public IActionResult CangkuDizhi()
+        {
+            return View();
+        }
+
+    }
+}
