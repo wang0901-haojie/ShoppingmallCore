0525b449b7031cde7f0521b889c64bfbf8fabd08
githubyanqinglong <79302494+githubyanqinglong@users.noreply.github.com>
2021-03-02 19:16:30 +0800

    闫庆龙

---
+++ ShoppingmallCore/Controllers/HotboomController.cs
@@ -0,0 +1,52 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ShoppingmallCore.Controllers
+{
+    public class HotboomController : Controller
+    {
+        //代购订单
+        public IActionResult daigoudingdan()
+        {
+            return View();
+        }
+        //转运订单
+        public IActionResult zhuanyundingdan()
+        {
+            return View();
+        }
+
+        //转运详情
+        public IActionResult zhuanyundingdanXQ(int id)
+        { 
+            return View();
+        }
+
+        //首页
+        public IActionResult ShouYe()
+        {
+            return View();
+        }
+
+        //个人信息
+        public IActionResult gerenxinxi()
+        {
+            return View();
+        }
+
+        //申请发货
+        public IActionResult shenqingchuku()
+        {
+            return View();
+        }
+
+        //填写地址
+        public IActionResult tianxiedizhi()
+        {
+            return View();
+        }
+    }
+}
